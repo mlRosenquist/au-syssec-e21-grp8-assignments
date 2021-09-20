fc74528e53854a4a1dd4497832897981d3ff82ec
MLR <mortenrosenquist@gmail.com>
2021-09-20 10:52:36 +0200

    Added parts of cbc requests

--- assignment-1/cbc-padding-oracle/main.py
+++ assignment-1/cbc-padding-oracle/main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
+import requests
+from requests import Session
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+url = 'http://localhost:5000'
+
+def getAuthToken():
+    # Get cookie
+    session = requests.session()
+    session.get(url)
+    cookies = session.cookies.get_dict()
+    return cookies['authtoken']
+
+def getQuote(authToken):
+    # Get cookie
+    cookies = {'authtoken': authToken}
+    r = requests.get(url+'/quote', cookies=cookies)
+
+    return r.text
+
+def manipulateAuthToken(initialToken, newByte, newByteIndex):
+    stringBytes = str.encode(initialToken)
+    print(stringBytes.hex())
+    stringBytes = stringBytes[0:newByteIndex] + newByte + stringBytes[newByteIndex+1:]
+    print(stringBytes.hex())
+    return stringBytes.decode()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+authToken = getAuthToken()
+print(authToken)
 
+newToken = manipulateAuthToken(authToken, b'\x24', 127)
+newToken = manipulateAuthToken(authToken, b'\x25', 126)
+newToken = manipulateAuthToken(authToken, b'\x26', 125)
+newToken = manipulateAuthToken(authToken, b'\x27', 124)
+print(newToken)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+quote = getQuote(authToken)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(quote)
