a8f7e9d769329163362a0b182738d78b180f6463
Valdemar Tang <vta@hmf.dk>
2021-09-23 08:18:58 +0200

    Pushed working test

--- assignment-1/cbc-padding-oracle/main.py
+++ assignment-1/cbc-padding-oracle/main.py
@@ -8,12 +8,13 @@ def getAuthToken() -> str:
     session = requests.session()
     session.get(url)
     cookies = session.cookies.get_dict()
-    return cookies['authtoken']
+    # return cookies['authtoken']
+    return '0c4746d2e40f6e2ee0a2818d8e2ef235ad41362341e19134c38e4d0150a31804b6b13c50586c408c9610afaa8cfac6ffc463ce17141b56bae954b76d576aded9a40389bab83cbdf31fc46227797b2fe0e8b64a03ccf313ebee17acf0bf8e36ee9dafd615c3d4df4e13fe959387c79905'
 
 def getQuote(authToken):
     # Get cookie
     cookies = {'authtoken': authToken}
-    print(authToken[len(authToken)-36-2:])
+    # print(authToken)
     r = requests.get(url+'/quote', cookies=cookies)
     return r.text
 
--- assignment-1/cbc-padding-oracle/test.py
+++ assignment-1/cbc-padding-oracle/test.py
@@ -34,6 +34,7 @@ BLOCK_SIZE = 16
 def recoverSecret():
     prefix_known_length = 26 #The known length of the message that preceeds the secret part
     token = bytearray.fromhex(getAuthToken())
+    print(token.hex())
     iv = token[0:16]
     token = token[16:] #First 16 bytes are IV
     recovered_plaintext = ""
@@ -41,20 +42,27 @@ def recoverSecret():
         prev_block_start = len(token)-(b*BLOCK_SIZE)-BLOCK_SIZE-1
         recovered_block = bytearray()
         padding = 0x01
-        for i in range(BLOCK_SIZE-1, -1, -1):
+        for i in range(BLOCK_SIZE-1, 0, -1):
             token_copy = token[:]
             for j in range(padding - 1): #Use new padding for existing recovered bytes
-                token_copy[prev_block_start + BLOCK_SIZE - 1 - j] = token_copy[prev_block_start + BLOCK_SIZE - 1 - j] ^ recovered_block[j] ^ padding
+                token_copy[prev_block_start + BLOCK_SIZE - j] = token_copy[prev_block_start + BLOCK_SIZE - j] ^ recovered_block[j] ^ padding
             byte_recovered = False
             for j in range(256):
-                token_copy[prev_block_start + i] = token_copy[prev_block_start + i] ^ j ^ padding
-                result = getQuote((iv + token_copy).hex())
+                if(j == 0x01):
+                    continue
+                original_value = token_copy[prev_block_start + i + 1]
+                token_copy[prev_block_start + i + 1] = token_copy[prev_block_start + i + 1] ^ j ^ padding
+                to_test = (iv + token_copy)[prev_block_start+16+1:prev_block_start+16+2*BLOCK_SIZE+1].hex()
+                result = getQuote(to_test)
                 if("No quote for you!" in result): #Correct byte found 
+                    print(to_test.upper())
                     recovered_block = recovered_block + j.to_bytes(1, sys.byteorder)
                     recovered_plaintext += chr(j)
-                    print(chr(j))
+                    print(f"Found {len(recovered_block)} bytes: {recovered_block[::-1].hex()}")
                     byte_recovered = True
                     break
+                else:
+                    token_copy[prev_block_start + i + 1] = original_value
             padding += 0x01
             if(not byte_recovered):
                 return ""
