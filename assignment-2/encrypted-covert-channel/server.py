0a586ba593a25e02bd69de5db4d3fd5e377a51d3
= <valdemar-tang@hotmail.com>
2021-10-21 21:53:54 +0200

    Updated server to properly decode messages on linux

--- assignment-2/encrypted-covert-channel/server.py
+++ assignment-2/encrypted-covert-channel/server.py
@@ -3,13 +3,17 @@ from login import login
 from hashlib import sha512
 from Crypto.Util.Padding import unpad
 from Crypto.Cipher import AES
-from icmp import receive
+from icmp import receive, PLATFORM_WINDOWS
 import os
 
 def decrypt(key, data) -> bytes :
-    nonce = data[0:12]
     tag = data[len(data)-16:len(data)]
-    data = data[12:len(data)-16]
+    if PLATFORM_WINDOWS:
+        nonce = data[0:12]
+        data = data[12:len(data)-16]
+    else:
+        nonce = data[20:32]
+        data = data[32:len(data)-16]
     cipher = AES.new(key, AES.MODE_GCM, nonce=nonce)
     return unpad(cipher.decrypt_and_verify(data, tag), 16)
 
@@ -23,7 +27,8 @@ key = bytearray.fromhex(open(f"{dir_path}/key.txt").read())
 serverSocket = soc.socket(soc.AF_INET, soc.SOCK_RAW, soc.IPPROTO_ICMP)
 localHost = soc.gethostbyname(soc.gethostname())
 serverSocket.bind((localHost, 0))
-serverSocket.ioctl(soc.SIO_RCVALL, soc.RCVALL_ON)
+if PLATFORM_WINDOWS:
+    serverSocket.ioctl(soc.SIO_RCVALL, soc.RCVALL_ON)
 print(f"Listening on: {localHost}")
 while True:
     data, addr = receive(serverSocket)
