c8bb222be58581b92c76c1c1cb8c494f7171ba82
Harfeld <andreas.harfeld@gmail.com>
2021-10-28 19:59:16 +0200

    server bidirectional

--- assignment-2/Mini_TLS-based_VPN_tunneling/tun_server.py
+++ assignment-2/Mini_TLS-based_VPN_tunneling/tun_server.py
@@ -3,6 +3,9 @@
 import fcntl
 from scapy.all import *
 
+CLIENT_IP = "10.0.2.4"
+CLIENT_PORT = 43102
+
 # Creating the tun interface
 TUNSETIFF = 0x400454ca
 IFF_TUN = 0x0001
@@ -10,7 +13,7 @@ IFF_TAP = 0x0002
 IFF_NO_PI = 0x1000
 
 tun = os.open("/dev/net/tun", os.O_RDWR)
-ifr = struct.pack('16sH', b'ahj%d', (IFF_TUN | IFF_NO_PI))
+ifr = struct.pack('16sH', b'server%d', (IFF_TUN | IFF_NO_PI))
 ifname_bytes = fcntl.ioctl(tun, TUNSETIFF, ifr)
 print("tun interface created...")
 
@@ -19,8 +22,9 @@ ifname = ifname_bytes.decode('UTF-8') [:16].strip('\x00')
 print("Interface Name: {}".format(ifname))
 
 # Setting up the server0 interface
-os.system("ip addr add 192.168.53.99/24 dev {}".format(ifname))
+os.system("ip addr add 192.168.53.98/24 dev {}".format(ifname))
 os.system("ip link set dev {} up".format(ifname))
+os.system("sudo sysctl net.ipv4.ip_forward=1")
 print("Interface is set up...")
 
 # Setting up socket interface
@@ -34,15 +38,15 @@ print("starting to listen:")
 while True:
 	# this will block until at least one interface is ready
 	ready, _, _ = select.select([sock, tun], [], [])
-    	for fd in ready:
-        	if fd is sock:
-        	    data, (ip, port) = sock.recvfrom(2048)
-        	    pkt = IP(data)
-        	    print("From socket <==: {} --> {}".format(pkt.src, pkt.dst))
-        	    os.write(tun, bytes(pkt))
-        
+	for fd in ready:
+		if fd is sock:
+			data, (ip, port) = sock.recvfrom(2048)
+			pkt = IP(data)
+			print("From {}:{} via socket <==: {} --> {}".format(ip, port, pkt.src, pkt.dst))
+			os.write(tun, bytes(pkt))
+
 		if fd is tun:
-        	    packet = os.read(tun, 2048)
-        	    pkt = IP(packet)
-        	    print("From tun ==>: {} --> {}".format(pkt.src, pkt.dst))
-        	    sock.sendto(packet, ("10.0.2.4", 9090))
+			packet = os.read(tun, 2048)
+			pkt = IP(packet)
+			print("From tun ==>: {} --> {}".format(pkt.src, pkt.dst))
+			sock.sendto(packet, (CLIENT_IP, CLIENT_PORT))
